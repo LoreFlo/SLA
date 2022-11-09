708f48b401f8864082ac2c04b7247ef78caa0611
LoreFlo <laurenalavanda@gmail.com>
2022-11-08 23:31:40 -0600

    modelos lineales de sla y tricomas

--- scripts/modelos_lineales.R
+++ scripts/modelos_lineales.R
@@ -3,30 +3,30 @@
 
 #getwd()
 
-if(!require(tidyverse)){install.packages("tidyverse")}; library(tidyverse)
+library(tidyverse)
+library(ggplot2)
 library(corrplot)
 library(lmtest)
-### es una funcion util para instalar y cargar paketes que se pueden necesitar o no. 
-
-peso_seco <- read.csv("./db/peso_seco.csv", colClasses = c("ciudad"="factor", "ambiente"="factor", "sitio"="factor", "parche"="factor"))
+ 
+#peso_seco <- read.csv("./db/peso_seco.csv", colClasses = c("ciudad"="factor", "ambiente"="factor", "sitio"="factor", "parche"="factor"))
 
 #tricomas <- read.csv("./db/tricomas.csv") #esta base se modificó y mas adelante se abre la version que sirve para este script
-npk <- read.csv(file = './db/npk.csv', colClasses = c("ciudad"="factor", "ambiente"="factor", "sitio"="factor", "parche"="factor"))
+#npk <- read.csv(file = './db/npk.csv', colClasses = c("ciudad"="factor", "ambiente"="factor", "sitio"="factor", "parche"="factor"))
 
 
 #################################################################################
 ###############  PROBANDO GRAFICAS EN R  ########################################
 #################################################################################
 
-## PESO SECO generales
+#### PESO SECO generales
 #head(peso_seco)
 #peso_seco %>% count(ID)
 #summary(peso_seco)
-hist(peso_seco$sla) ### así queda con distribución sesgada
-hist(log(peso_seco$sla)) ### así queda con distribucion normal
-qqnorm(log(peso_seco$sla))
+#hist(peso_seco$sla) ### así queda con distribución sesgada
+#hist(log(peso_seco$sla)) ### así queda con distribucion normal
+#qqnorm(log(peso_seco$sla))
 
-## TRICOMAS
+#### TRICOMAS
 #head(tricomas)
 #tricomas %>% count(ID)
 #summary(tricomas)
@@ -40,44 +40,46 @@ qqnorm(log(peso_seco$sla))
 #  sep = ",", 
 #  col.names = T, row.names = F)
 
-trico <- read.csv("./db/prom_tricomas.csv", colClasses = c("ciudad"="factor", "ambiente"="factor"))
-str(trico)
-hist(log(trico$tricomas)) #con log queda menos sesgada la distribucion
-qqnorm(log(trico$tricomas))
+#trico <- read.csv("./db/prom_tricomas.csv", colClasses = c("ciudad"="factor", "ambiente"="factor"))
+#str(trico)
+#hist(log(trico$tricomas)) #con log queda menos sesgada la distribucion
+#qqnorm(log(trico$tricomas))
 
-### fuente https://www.youtube.com/watch?v=BffAQZzbBms&t=3873s
+#### fuente https://www.youtube.com/watch?v=BffAQZzbBms&t=3873s
 
-## NPK generales
+#### NPK generales
 #head(npk)
 #npk %>% count(ID)
 #summary(npk)
-hist(log(npk$P_mg_Kg))
-hist(log(npk$C_porcentaje))
-hist(npk$K_Cmol_Kg)
-hist(npk$N_porcentaje)
+#hist(log(npk$P_mg_Kg))
+#hist(log(npk$C_porcentaje))
+#hist(npk$K_Cmol_Kg)
+#hist(npk$N_porcentaje)
 
-qqnorm(npk$C_porcentaje)
-qqnorm(npk$N_porcentaje)
+#qqnorm(npk$C_porcentaje)
+#qqnorm(npk$N_porcentaje)
 
 
 ########## Gráfica de dispersión ##########
 
-plot(log(peso_seco$sla), prom_tri$tricomas)
-plot(npk$C_porcentaje, log(peso_seco$sla))
-plot(npk$N_porcentaje, log(peso_seco$sla))
+#plot(log(peso_seco$sla), prom_tri$tricomas)
+#plot(npk$C_porcentaje, log(peso_seco$sla))
+#plot(npk$N_porcentaje, log(peso_seco$sla))
 
 
 ####################  GRAFICO DE CORRELACIONES  ############################### 
-# https://cran.r-project.org/web/packages/corrplot/vignettes/corrplot-intro.html
+#### https://cran.r-project.org/web/packages/corrplot/vignettes/corrplot-intro.html
 
-cap1 <- read.csv("./db/cap1_str.csv", colClasses = c("ciudad"="factor", "ambiente"="factor", "sitio"="factor", "parche"="factor"))
+cap1 <- read.csv("./db/cap1_str.csv", colClasses = 
+                   c("ciudad"="factor", "ambiente"="factor", "sitio"="factor", "parche"="factor",
+                     "sla"="double"))
 c1<-cor(cap1[,13:18], use = "complete.obs")
 c1
 corrplot(c1, method = "number")  # si está muy bajito el color de los numeros, quitar method
 
-######################################
-########## Linear Regresion ##########
-######################################
+#########################################################################################
+################################### Linear Regresion ##########
+#########################################################################################
 
 # regresion lineal <- dependiente (cuantitativa continua); independiente (mixtas)
 # objetivo: predecir la variable depediente Y en funcion de un conjunto de variables independientes Xj
@@ -86,29 +88,29 @@ corrplot(c1, method = "number")  # si está muy bajito el color de los numeros, 
 # residuo: (error) Y-y
 
 
-m_a <- lm(log(peso_seco$sla)~ peso_seco$ambiente)
+m_a <- lm(log(sla)~ ambiente, data = cap1)
 summary(m_a)
-# residuals: la mediana está cerca del cero, parece que el cuartil 1 y 3 estan equidistantes.
+# residuals: la mediana está cerca del cero 0.00618, parece que el cuartil 1 (-0.20283) y 3 (0.19112) estan equidistantes.
 # el modelo tiene intercepto (por eso salio significativo, lo cual es importante si me interesa hacer un modelo predictivo)
 # las diferencias entre ambientes son significativas (p>0.001)
-# 15% de la variabilidad del sla se explica por el ambienteu (Multiple R-squared:  0.1559).
+# 15% de la variabilidad del log(sla) se explica por el ambienteu (Multiple R-squared: 0.1559).
 # F-statistic: 26.79 on 1 and 145 DF,  p-value: 7.443e-07
 
 #NOTAS sobre lm
 # Residual standard error: 0.3213 (la variabilidad que no predice el modelo)
 # R^2<-variabilidad explicada por el modelo / variabilidad total
-# Multiple R-squared:  0.1559 (la ajustada es para mas de una predictora)
-# Para interpretar más fácil : 0.1559*100= 15.59% de variabilidad explicada por el modelo
-# el estadistico F (o prueba de hipótesis) dice si la variailidad de grupos es mayor que
+# Multiple R-squared:  0.1623 (la ajustada es para mas de una predictora)
+# Para interpretar más fácil : 0.1623*100= 16.23% de variabilidad explicada por el modelo
+# el estadistico F (o prueba de hipótesis) dice si la variabilidad de grupos es mayor que
 # la variabilidad intragrupos.
 
-# EJERCICIO: Predecir un valor de sla para una planta con sla de 20
-m_a #coeficientes: intercept 3.3045; ambienteu 0.2746
+# EJERCICIO VALIDO PARA VAR CONTINUAS: Predecir un valor de sla para una planta con sla de 20
+# m_a #coeficientes: intercept 28.254; ambienteu 9.812
 # beta0 es el intercepto
 # beta1 es el coeficiente de mi primer variable independiente, que en este caso es ambienteu
 # siguiendo el modelo y=beta1*x1+beta0 :
-#sla_20<- 0.2746*20+3.3045
-#sla_20 Me sale 8.7, pero no tiene ningun sentido, tendría que predecir a que categoria pertenece ese tamaño de sla...
+#sla_20<- 9.812*20+28.254
+#sla_20 Me sale 224.494, pero no tiene ningun sentido, tendría que predecir a que categoria pertenece ese tamaño de sla...
 
 anova(m_a)
 #anova descompone la variabilidad, por ejemplo la var que predice el valor de sla
@@ -125,24 +127,21 @@ confint(m_a) # da los intervalos de confianza del beta0 y beta1.
 
 ########## Modelo de regresión múltiple ##########
 
-?glm # regresion lineal, logistica, gaussiana, binomial...
-
-m_ac <- lm(sla ~ ambiente+ciudad, data = peso_seco)
+m_ac <- lm(log(sla) ~ ambiente+ciudad, data = cap1)
 m_ac
 summary(m_ac)
-# mediana negativa, q 1 y 3 no son equidistantes
-# coeficiente beta para ambienteU y para ciudadcam 
-# ambienteU, ciudadCam, estadísticamente significativo con un alfa de 0.001
-# Adjusted R-squared:  0.2153 
-# F-statistic: 9.011, p-value: 1.883e-07
+# mediana cerca de cero (0.02753), q 1 (-0.17886) y 3 (0.18364)  equidistantes
+# ambienteU, y el intercepto, estadísticamente significativo con un alfa de 0.001
+# Adjusted R-squared:  0.1848 
+# F-statistic: 7.621 on 5 and 141 DF,  p-value: 2.295e-06
 
 ########### Contribuira mucho la var. ciudad o no? Comparar modelos con anova #####
 
-anova(m_a, m_ac) # deberia dar una tabla de analisis de varianza comparando modelos. NO ME SALE.
+anova(m_a, m_ac) # da una tabla de analisis de varianza comparando modelos. 
 # está comparando si la diferencia en la suma de cuadrados es estadisticamente 
 # significativa. El modelo que sale significativo es mejor que el otro en términos de 
-# disminucion de l suma de cuadrados.
-
+# disminucion de la suma de cuadrados.
+# en este caso fue el modelo 2 (m_ac) p<0.05
 
 ########## Comparación de modelos ################
 
@@ -152,9 +151,9 @@ anova(m_a, m_ac) # deberia dar una tabla de analisis de varianza comparando mode
 # Estos valores tienen sentido cuando se contrastan
 
 delta.AIC<- AIC(m_a)-AIC(m_ac)
-delta.AIC
+delta.AIC # 2.246209
 delta.BIC<- BIC(m_a)-BIC(m_ac)
-delta.BIC
+delta.BIC # -9.715522
 
 ############ Validacion de supuestos ###############
 
@@ -167,6 +166,58 @@ delta.BIC
 ## 1. Varianza constante (Homocedasticidad; que el modelo se equivoque igual para valores altos o bajos)
 ## 2. Normalidad conjunta (de los errores)
 ## 3. Independencia de los errores 
+
+# Linealidad
+## Prueba de falta de ajuste
+## Explorar términos polinómicos mediante polinomios ortogonales
+#m1<-lm(sla~poly(ambiente,3), data = cap1) # expresion no apta para mis variables predictoras que son categoricas
+#summary(m1)
+#### se supone que si ningun polinomio funciona tan bien como el lineal, es una confirmación de 
+#### la exploracion que se hizo previamente con graficas como la siguiente:
+#cap1 %>%
+  #ggplot(aes(x=ambiente, y=log(sla)))+geom_point()+geom_smooth(method="lm")+
+  #theme_classic()+ylab("specific leaf area")+xlab(ambiente)
+#?cor
+#c1<-cor(cap1$sla, cap1$ambiente, method = "spearman")
+
+######## FIN DEL CURSO (EL RESTO SE PUEDE SEGUIR SOLO CON VAR continuas) ###########
+######## CONTINUO CON LAS VARIABLES DE RESPUESTA QUE ME FALTAN #######################
+
+m_acac <- lm(log(sla) ~ ambiente+ciudad+(ambiente*ciudad), data = cap1)
+summary(m_acac) # m 0.03334 1Q  -0.20489 3Q 0.16153
+# intercepto p<0.001 y ambienteU p<0.01
+# Residual standard error: 0.3182; Multiple R-squared:  0.2177,	Adjusted R-squared:  0.1663 
+# F-statistic: 4.237 on 9 and 137 DF,  p-value: 7.367e-05
+
+
+##################### LM TRICOMAS ##########################
+t_a <- lm(log(tricomas)~ambiente, data = cap1)
+t_a # intercepto 9.099 , ambienteU -4.169
+summary(t_a) # med -0.02073, 1Q -0.51321, 3Q 0.44753
+# ambienteU p<0.001; Residual standard error: 0.8044; Multiple R-squared:  0.1001
+#F-statistic: 15.68 on 1 and 141 DF,  p-value: 0.0001187
+
+t_ac <- lm(log(tricomas)~ambiente+ciudad, data = cap1)
+summary(t_ac) # med 0.03467; 1Q -0.47695; 3Q 0.47792
+# ciudad cam, ambienteU, ciudadMid, ciudadVal, todos con p<0.001
+# Residual standard error: 0.71
+#F-statistic: 12.82 on 5 and 137 DF,  p-value: 3.161e-10
+
+t_acac <- lm(log(tricomas)~ambiente+ciudad+(ambiente*ciudad), data = cap1)
+summary(t_acac)
+# med 0.04853; 1Q -0.39717; 3Q 0.42373
+# intercepto, ambienteU, cun, tiz, val, ambienteU*cun tienen p<0.001
+# cun, ambienteU*tiz, tienen p<0.01
+# Residual standard error: 0.6397 on 133 degrees of freedom
+# Multiple R-squared:  0.463,	Adjusted R-squared:  0.4267 
+# F-statistic: 12.74 on 9 and 133 DF,  p-value: 1.832e-14
+
+
+
+
+
+
+
 
 
 
